c5621116e5659badeed385646c3d7d9dfbbd2428
Skybailan <3236394293@qq.com>
2026-04-16 21:20:10 +0800

    Add files via upload

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'status': 'ok', 'service': 'django-ij21'})),
+    path('healthz', lambda request: JsonResponse({'status': 'ok'})),
     path('admin/', admin.site.urls),
     path('api/', include('words.urls')),
 ]
